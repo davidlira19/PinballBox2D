1eb23f9491e2fa27e28ded27cfe37deb0c31dc07
adrianam4 <61042878+adrianam4@users.noreply.github.com>
2020-11-09 19:29:13 +0100

    bars movement (just textures)

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -1,6 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModulePlayer.h"
+#include "ModuleInput.h"
 
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -31,10 +32,31 @@ bool ModulePlayer::CleanUp()
 // Update: draw background
 update_status ModulePlayer::Update()
 {
-	App->renderer->Blit(grandeder, 246, 814);
-	App->renderer->Blit(grandeizq, 145, 818);
-	App->renderer->Blit(pequenaizq, 137, 730);
-	App->renderer->Blit(pequenader, 281, 720);
+	//LEFT BARS TEXTURES
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	{
+		App->renderer->Blit(grandeizq, 145, 818,0,1.0f,(0,-50),10,15);
+		App->renderer->Blit(pequenaizq, 137, 730, 0, 1.0f, (0, -50),10,15);
+	}
+	else 
+	{
+		App->renderer->Blit(grandeizq, 145, 818);
+		App->renderer->Blit(pequenaizq, 137, 730);
+	}
+
+	//RIGHT BARS TEXTURES
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	{
+		App->renderer->Blit(grandeder, 240, 818, 0, 1.0f, (0, 50), 50, 5);
+		App->renderer->Blit(pequenader, 271, 730, 0, 1.0f, (0, 50), 50, 15);
+	}
+	else
+	{
+		App->renderer->Blit(grandeder, 240, 818);
+		App->renderer->Blit(pequenader, 275, 730);
+	}
+
+	
 	return UPDATE_CONTINUE;
 }
 
